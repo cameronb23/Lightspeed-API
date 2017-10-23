7de49989706e8f3d6914be3763df00d5f1114eb4
cameronb23 <cbutler2018@gmail.com>
2017-10-23 09:37:49 -0400

    send response

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.post('/emailInfo', (req, res) => {
     }
   
     console.log('Message sent: %s', info.messageId);
-    res.status(200);
+    res.status(200).send('Message sent');
   });
 });
 
