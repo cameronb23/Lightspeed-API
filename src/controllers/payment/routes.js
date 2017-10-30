415bb4168f66c563e52f0024eafe8fcec1df170b
Cameron <cbutler2018@gmail.com>
2017-10-29 21:43:09 -0400

    Stripe testing.

--- src/controllers/payment/routes.js
+++ src/controllers/payment/routes.js
@@ -8,11 +8,31 @@ const stripe = stripeSetup('sk_test_0MNTf884MCmC930BkoQQ3rBI');
 
 
 router.post('/create', (req, res) => {
-  if (!req.body.stripeToken || !req.body.productId) {
+  if (!req.body.stripeToken) {
     return res.status(400).send({
       success: false,
       message: 'Invalid payload'
     });
   }
 
+  stripe.charges.create({
+    amount: 1000,
+    currency: "usd",
+    description: "Test charge",
+    source: req.body.stripeToken,
+  }, function(err, charge) {
+    // asynchronously called
+    console.log(charge);
+    if (err) {
+      return res.status(500).send({
+        success: false,
+        message: 'Error charging card.'
+      });
+    }
+
+    return res.status(200).send({
+      success: true,
+      message: 'Payment success'
+    });
+  });
 })
